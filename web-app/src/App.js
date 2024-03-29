26701e3aa1991b1a5cfb80c2b41d6f97ea3b77e6
RockRar484 <czileen484patel@gmail.com>
2024-03-29 19:37:58 +0530

    router init

--- web-app/src/App.js
+++ web-app/src/App.js
@@ -1,12 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+// import Home from './pages/Home';
+// import CreateBook from './pages/CreateBooks';
+// import ShowBook from './pages/ShowBook';
+// import EditBook from './pages/EditBook';
+// import DeleteBook from './pages/DeleteBook';
+import RegisterForm from './pages/register';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-
-    </div>
+    <Routes>
+      <Route path='/' element={<RegisterForm />} />
+      {/* <Route path='/books/create' element={<CreateBook />} />
+      <Route path='/books/details/:id' element={<ShowBook />} />
+      <Route path='/books/edit/:id' element={<EditBook />} />
+      <Route path='/books/delete/:id' element={<DeleteBook />} /> */}
+    </Routes>
   );
-}
+};
 
 export default App;
--- web-app/src/index.js
+++ web-app/src/index.js
@@ -2,14 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import LoginForm from './login'
-import RegisterForm from './register'
+import LoginForm from './pages/login'
+import RegisterForm from './pages/register'
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <RegisterForm />
+    <BrowserRouter>
+
+      <App></App>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
