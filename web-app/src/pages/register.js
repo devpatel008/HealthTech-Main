b635ddac6a13d6783ca9e1d7a6fbce0f5dfa9b01
SHARVIL-18 <4224.stkabirdin@gmail.com>
2024-03-30 11:38:58 +0530

    register_frontend

--- web-app/src/pages/register.js
+++ web-app/src/pages/register.js
@@ -1,25 +1,19 @@
-// src/components/RegisterForm.js
-
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+
 const RegisterForm = () => {
     const navigate = useNavigate();
     const [name, setName] = useState('');
     const [password, setPassword] = useState('');
     const [email, setEmail] = useState('');
-    // const [confirmPassword, setConfirmPassword] = useState('');
     const [error, setError] = useState('');
 
     const handleSubmit = async (e) => {
         e.preventDefault();
 
-        // Backend URL - Replace with your actual backend URL
         const backendUrl = 'http://localhost:4000/api/v1/auth/register';
 
         try {
-            // if (password !== confirmPassword) {
-            //     throw new Error('Passwords do not match');
-            // }
             const response = await fetch(backendUrl, {
                 method: 'POST',
                 headers: {
@@ -32,7 +26,6 @@ const RegisterForm = () => {
                 throw new Error('Registration failed');
             }
 
-            // Redirect or perform other actions upon successful registration
             console.log('Registration successful');
             
             navigate('/');
@@ -42,46 +35,141 @@ const RegisterForm = () => {
     };
 
     return (
-        <div>
-            <h2>Register</h2>
-            {error && <p>{error}</p>}
-            <form onSubmit={handleSubmit}>
-                <div>
-                    <label>Email:</label>
-                    <input
-                        type="text"
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                    />
-                </div>
-                <div>
-                    <label>name:</label>
-                    <input
-                        type="text"
-                        value={name}
-                        onChange={(e) => setName(e.target.value)}
-                    />
-                </div>
-                <div>
-                    <label>Password:</label>
-                    <input
-                        type="password"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                    />
-                </div>
-                {/* <div>
-                    <label>Confirm Password:</label>
-                    <input
-                        type="password"
-                        value={confirmPassword}
-                        onChange={(e) => setConfirmPassword(e.target.value)}
-                    />
-                </div> */}
-                <button type="submit">Register</button>
-            </form>
-        </div>
+        <>
+            <style>
+                {`
+                    *,
+                    *:before,
+                    *:after {
+                        padding: 0;
+                        margin: 0;
+                        box-sizing: border-box;
+                    }
+                    body {
+                        background-color: #080710;
+                    }
+                    .background {
+                        width: 430px;
+                        height: 520px;
+                        position: absolute;
+                        transform: translate(-50%, -50%);
+                        left: 50%;
+                        top: 50%;
+                    }
+                    .background .shape {
+                        height: 200px;
+                        width: 200px;
+                        position: absolute;
+                        border-radius: 50%;
+                    }
+                    .shape:first-child {
+                        background: linear-gradient(#1845ad, #23a2f6);
+                        left: -80px;
+                        top: -80px;
+                    }
+                    .shape:last-child {
+                        background: linear-gradient(to right, #ff512f, #f09819);
+                        right: -30px;
+                        bottom: -80px;
+                    }
+                    form {
+                        height: 520px;
+                        width: 400px;
+                        background-color: rgba(255, 255, 255, 0.13);
+                        position: absolute;
+                        transform: translate(-50%, -50%);
+                        top: 50%;
+                        left: 50%;
+                        border-radius: 10px;
+                        backdrop-filter: blur(10px);
+                        border: 2px solid rgba(255, 255, 255, 0.1);
+                        box-shadow: 0 0 40px rgba(8, 7, 16, 0.6);
+                        padding: 50px 35px;
+                    }
+                    form * {
+                        font-family: 'Poppins', sans-serif;
+                        color: #ffffff;
+                        letter-spacing: 0.5px;
+                        outline: none;
+                        border: none;
+                    }
+                    form h2 {
+                        font-size: 32px;
+                        font-weight: 500;
+                        line-height: 42px;
+                        text-align: center;
+                    }
+                    label {
+                        display: block;
+                        margin-top: 30px;
+                        font-size: 16px;
+                        font-weight: 500;
+                    }
+                    input {
+                        display: block;
+                        height: 50px;
+                        width: 100%;
+                        background-color: rgba(255, 255, 255, 0.07);
+                        border-radius: 3px;
+                        padding: 0 10px;
+                        margin-top: 15px;
+                        font-size: 14px;
+                        font-weight: 300;
+                    }
+                    ::placeholder {
+                        color: #e5e5e5;
+                    }
+                    button {
+                        margin-top: 30px;
+                        width: 100%;
+                        background-color: #ffffff;
+                        color: #080710;
+                        padding: 15px 0;
+                        font-size: 18px;
+                        font-weight: 600;
+                        border-radius: 5px;
+                        cursor: pointer;
+                    }
+                `}
+            </style>
+            <div className="background">
+                <div className="shape"></div>
+                <div className="shape"></div>
+            </div>
+            <div>
+                <h2>Register</h2>
+              
+                <form onSubmit={handleSubmit}>
+                {error && <p>{error}</p>}
+                    <div>
+                        <label>Email:</label>
+                        <input
+                            type="text"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                        />
+                    </div>
+                    <div>
+                        <label>Name:</label>
+                        <input
+                            type="text"
+                            value={name}
+                            onChange={(e) => setName(e.target.value)}
+                        />
+                    </div>
+                    <div>
+                        <label>Password:</label>
+                        <input
+                            type="password"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                        />
+                    </div>
+                    <button type="submit">Register</button>
+                </form>
+            </div>
+        </>
     );
 };
 
-export default RegisterForm;
+export default RegisterForm;
